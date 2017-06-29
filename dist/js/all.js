10df98406fdcf1a80bf58b473a3af210730d7966
engine063 <916529626@qq.com>
2017-06-29 19:27:11 +0800

    'initial'

---
+++ dist/js/all.js
@@ -0,0 +1,7 @@
+var log=function(o){console.log("--------"),console.log(o),console.log("--------")};log({a:1}),log("gulp-book");
+//# sourceMappingURL=log.js.map
+
+var say=function(o){o>0&&(o--,console.log(o),say(o))};say(10),console.log("------------"),say(5);
+//# sourceMappingURL=say.js.map
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImxvZy5qcyIsInNheS5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUNGQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYWxsLmpzIiwic291cmNlc0NvbnRlbnQiOlsidmFyIGxvZz1mdW5jdGlvbihvKXtjb25zb2xlLmxvZyhcIi0tLS0tLS0tXCIpLGNvbnNvbGUubG9nKG8pLGNvbnNvbGUubG9nKFwiLS0tLS0tLS1cIil9O2xvZyh7YToxfSksbG9nKFwiZ3VscC1ib29rXCIpO1xuLy8jIHNvdXJjZU1hcHBpbmdVUkw9bG9nLmpzLm1hcFxuIiwidmFyIHNheT1mdW5jdGlvbihvKXtvPjAmJihvLS0sY29uc29sZS5sb2cobyksc2F5KG8pKX07c2F5KDEwKSxjb25zb2xlLmxvZyhcIi0tLS0tLS0tLS0tLVwiKSxzYXkoNSk7XG4vLyMgc291cmNlTWFwcGluZ1VSTD1zYXkuanMubWFwXG4iXX0=
